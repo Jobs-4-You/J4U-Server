59ae18a3c77d589296de37dab4ceaa59d4a67e90
JimZer <jimi.vaubien@protonmail.com>
2018-10-04 11:11:30 +0200

    Login process started

--- server/database/models.py
+++ server/database/models.py
@@ -1,15 +1,18 @@
-from sqlalchemy import Column, Integer, String
+from flask_login import UserMixin
+from sqlalchemy import Column, Integer, String, Boolean
 from database.database import Base
 
-class User(Base):
+class User(UserMixin, Base):
   __tablename__ = 'users'
   id = Column(Integer, primary_key=True)
-  name = Column(String(50), unique=True)
+  name = Column(String(50))
   email = Column(String(120), unique=True)
+  form_done = Column(Boolean())
 
-  def __init__(self, name=None, email=None):
+  def __init__(self, name=None, email=None, form_done=False):
     self.name = name
     self.email = email
+    self.form_done = form_done
 
   def __repr__(self):
-    return '<User %r>' % (self.name)
+    return '<User {} {} {}>'.format(self.name, self.email, self.form_done)
--- server/server.py
+++ server/server.py
@@ -1,14 +1,39 @@
 from flask import Flask
+from flask_login import LoginManager, current_user, login_user
 from database.database import init_db, db_session
+from database.models import User
 
 app = Flask('J4U-Server')
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
-  db_session.remove()
+    db_session.remove()
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
 
 if __name__ == "__main__":
-  init_db()
-  app.run()
+    init_db()
+    app.run()
