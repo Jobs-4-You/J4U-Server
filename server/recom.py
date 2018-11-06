46d3d552ce7319065366a847a28f8cd8dc58fa84
JimZer <jimi.vaubien@protonmail.com>
2018-11-06 01:48:59 +0100

    recommendations route

---
+++ server/recom.py
@@ -0,0 +1,125 @@
+import pandas as pd
+import numpy as np
+from collections import Counter
+from scipy.spatial.distance import squareform, pdist
+
+import warnings
+warnings.filterwarnings("ignore")
+
+def compute_dist(quest_point, past_occup, alpha, beta) : 
+    
+    # Read the Data
+    data = pd.read_csv('onetXmauro2.csv', encoding='latin1')
+
+    # Rename the column TitreFR to Occupation
+    data.rename(columns = {'TitreFR' : 'Occupation'}, inplace = True)
+
+    # Drop the useless columns
+    data.drop(columns = [u'ElementID', u'isco08'], inplace = True)
+
+    # Set the occupation name as index of the dataFrame
+    data.set_index('Occupation', inplace = True)
+
+    # Drop the 'Membres des corps législatifs' job
+    data.drop(u'Membres des corps législatifs', inplace = True)
+
+    # Replce the missing ElementName of the 'JobZones' Domain
+    data['ElementName'] = data['ElementName'].fillna(value = 'JobZones')
+
+    # Drop the missing rows from ElementName
+    data.dropna(subset=[u'ElementName'], inplace = True)
+
+
+    domains = pd.unique(data['Domain'])
+
+    # Create a dictionary that will contain the df of the different domains
+    dfs = {}
+    for domain in domains :
+        dfs[domain] = data.loc[data['Domain'] == domain]
+
+
+    # Create the new rows of the dataFrame for the questionnaire
+    questionnaire_df = dfs[u'MM'].copy().reset_index().iloc[:12]
+    questionnaire_df['Occupation'] = u'Questionnaire'
+    questionnaire_df['DataValue'] = quest_point
+    questionnaire_df.set_index('Occupation', inplace = True)
+    dfs[u'MM'] = dfs[u'MM'].append(questionnaire_df)
+
+
+    distance_dfs = {}
+
+    for domain in dfs.keys() : 
+        
+            
+        # Get the DataFrame of the current Domain
+        df = dfs[domain].copy()
+        # Drop the useless Columns and reset the index
+        df.drop(columns = ['ElementName', 'Domain'], inplace = True)
+        df.reset_index(inplace = True)
+
+        # Create the new index on which we will pivot the dataFrame
+        nb_elem_byOccup = len(df.loc[df['Occupation'] == df['Occupation'][0]])
+        nb_occupations = len(pd.unique(df['Occupation']))
+        df['new_index'] = list(range(nb_elem_byOccup)) * nb_occupations
+
+        # Pivot the DataFrame such that we have the occupation on the columns
+        df = df.pivot(index = 'new_index', columns = 'Occupation', values = 'DataValue').T
+
+        # Compute the normalized pairwise distance between the occupations 
+        dist_df = pd.DataFrame(squareform(pdist(df, 'euclidean')), index=df.index, columns= df.index)
+        
+        # If domain is MM normalize later
+        if not(domain == u'MM') :
+            dist_df = (dist_df - np.mean(dist_df.as_matrix())) / np.std(dist_df.as_matrix())    
+        
+        distance_dfs[domain] = dist_df
+
+
+    # Sum of values for all pairwise Occupations of all the domains except MM
+    sum_domains = sum([distance_dfs[domain] for domain in domains if not domain == u'MM'])
+
+    # Sum of values for all pairwise Occupations of the MM domain and drop the row and column questionnaire
+    D_mmP = distance_dfs[u'MM']
+    D_mmP = D_mmP.drop(['Questionnaire']).drop(columns = ['Questionnaire'])
+    D_mmP = (D_mmP - np.mean(D_mmP.as_matrix())) / np.std(D_mmP.as_matrix())    
+
+    # Sum of values for all Occupations of the MM domain to the questionnaire values
+    D_mmS = distance_dfs[u'MM'].loc['Questionnaire'].copy()
+    del D_mmS['Questionnaire']
+    D_mmS = (D_mmS - np.mean(D_mmS)) / np.std(D_mmS)    
+
+    final_dist = (alpha * D_mmS + (1-alpha) * D_mmP[past_occup] + beta * sum_domains[past_occup]) / (1 + beta * 9)
+    final_dist = final_dist.sort_values()
+
+    
+    return list(final_dist.index)[:20], np.round(final_dist.values, 3)
+
+def recom(*var):
+    # Slider values from 1 to 5
+    list_var = [
+        var[0], var[1], var[2], var[3], var[4], var[5], var[6], var[7], var[8], var[9], var[10],
+        var[11]
+    ]
+    list_var = [var / 100 for var in list_var]
+
+    # The weight Importance of the old Position values
+    weight_oldPos_alpha = var[12] / 100
+
+    abilitiesImportance_beta = var[14] / 100
+
+    oldPos = var[13]
+
+    print(list_var, oldPos, weight_oldPos_alpha, abilitiesImportance_beta)
+
+    # Return the list of jobs that are the closest to the parameters we did input
+    res = compute_dist(
+        list_var, oldPos, weight_oldPos_alpha, abilitiesImportance_beta)
+    print('Done ! ')
+
+    return {
+        "HTTP Code": "200",
+        'message': 'All argument passed',
+        'vars': list_var,
+        'jobs': list(res[0]),
+        'importance': list(res[1])
+    }
--- server/server.py
+++ server/server.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, abort, jsonify
 from flask_login import LoginManager, current_user, login_user
+from flask_cors import CORS
 from database.database import init_db, db_session
 from database.models import User
+from recom import recom
 
 app = Flask('J4U-Server')
 app.secret_key = 'super secret key'
+CORS(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -27,9 +30,6 @@ def login():
     form = request.form.to_dict()
     print(form)
     user = User.query.filter_by(email=form['email']).first()
-    print('-'*100)
-    print(user)
-    print('-'*100)
     if user is None or not user.check_password(form['password']):
         abort(400, 'Invalid email or password')
     #login_user(user, remember=form.remember_me.data)
@@ -46,6 +46,32 @@ def logout():
     db_session.commit()
     logout_user()
     return jsonify(success=True)
+
+
+@app.route('/recom', methods=['GET'])
+def recomend():
+    # Abilities values
+    params = [
+        int(request.args.get('var1')),
+        int(request.args.get('var2')),
+        int(request.args.get('var3')),
+        int(request.args.get('var4')),
+        int(request.args.get('var5')),
+        int(request.args.get('var6')),
+        int(request.args.get('var7')),
+        int(request.args.get('var8')),
+        int(request.args.get('var9')),
+        int(request.args.get('var10')),
+        int(request.args.get('var11')),
+        int(request.args.get('var12')),
+        int(request.args.get('var13')),
+        str(request.args.get('var14')),
+        int(request.args.get('var15')),
+    ]
+    print(params)
+    res = recom(*params)
+
+    return jsonify(res)
 
 
 if __name__ == "__main__":
