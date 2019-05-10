b9131c90448c8ca020972627b118e3c45857c2c1
Lucas Rodrigues <lrodri@terpmail.umd.edu>
2019-05-10 20:59:55 +0200

    Updating admin script

--- server/server.py
+++ server/server.py
@@ -402,7 +402,6 @@ def user_infos():
 def updategroup():
     admin_password = get_config()['admin_pword']
     password = request.json['password']
-    print(admin_password)
     field = request.json['field']
     value = request.json['value']
     group = request.json['group']
