dc2fb5142460bc92d49a722f6fcc5a10e948aafc
JimZer <jimi.vaubien@protonmail.com>
2019-05-15 21:40:09 -0400

    .

--- server/config.py
+++ server/config.py
@@ -1,35 +1,35 @@
 import os
 
 dev = {
-    'mode': 'dev',
-    'url': 'http://127.0.0.1:5000',
-    'app_url': 'http://127.0.0.1:8080',
-    'host': '0.0.0.0',
-    'port': 5000,
+    "mode": "dev",
+    "url": "http://127.0.0.1:5000",
+    "app_url": "http://127.0.0.1:8080",
+    "host": "0.0.0.0",
+    "port": 5000,
 }
 
 prod = {
-    'mode': 'prod',
-    'url': 'https://j4u.unil.ch:5000',
-    'app_url': 'https://j4u.unil.ch',
-    'host': '127.0.0.1',
-    'port': 3000
+    "mode": "prod",
+    "url": "https://j4u.unil.ch:5000",
+    "app_url": "https://j4u.unil.ch",
+    "host": "127.0.0.1",
+    "port": 3000,
 }
 
 
 def get_config():
-    if os.environ.get('ENV') == 'prod':
-        conf =  prod
+    if os.environ.get("ENV") == "prod":
+        conf = prod
     else:
         conf = dev
 
-    conf['email_user'] = os.environ.get('MAIL_USER')
-    conf['email_pwd'] = os.environ.get('MAIL_PWD')
-    conf['app_key'] = os.environ.get('APP_KEY')
-    conf['salt'] = os.environ.get('SALT')
-    conf['jwt_key'] = os.environ.get('JWT_KEY')
-    conf['admin_pword']= os.environ.get('ADMIN_PWORD')
-    conf['mysql_user']= os.environ.get('MYSQL_USER')
-    conf['mysql_pwd']= os.environ.get('MYSQL_PWD')
+    conf["email_user"] = os.environ.get("MAIL_USER")
+    conf["email_pwd"] = os.environ.get("MAIL_PWD")
+    conf["app_key"] = os.environ.get("APP_KEY")
+    conf["salt"] = os.environ.get("SALT")
+    conf["jwt_key"] = os.environ.get("JWT_KEY")
+    conf["admin_pword"] = os.environ.get("ADMIN_PWORD")
+    conf["mysql_user"] = os.environ.get("MYSQL_USER")
+    conf["mysql_pwd"] = os.environ.get("MYSQL_PWD")
 
     return conf
--- server/qualtrics.py
+++ server/qualtrics.py
@@ -19,112 +19,116 @@ def min_max_scaler(domain, target, x):
 
 
 name_to_var = {
-    'Sc_Fluency': ('var1', partial(min_max_scaler, [0, 30], [1, 5])),
-    'Sc_Induc_Reas': ('var2', partial(min_max_scaler, [0, 8], [1, 5])),
-    'score_matrices': ('var3', partial(min_max_scaler, [0, 1], [1, 5])),
-    'Sc_WM': ('var4', partial(min_max_scaler, [0, 12], [1, 5])),
-    'Sc_MOY_RT': ('var5', partial(min_max_scaler, [0, 5], [1, 5])),
-    'Sc_Verbal_Com': ('var6', partial(min_max_scaler, [0, 48], [1, 5])),
-    'score_PM': ('var7', partial(min_max_scaler, [0, 1], [1, 5])),
-    'score_metacog': ('var8', partial(min_max_scaler, [0, 1], [1, 5])),
-    'Sc_Leader': ('var9', partial(min_max_scaler, [6, 24], [1, 5])),
-    'Sc_Self_cont': ('var10', partial(min_max_scaler, [13, 65], [1, 5])),
-    'Sc_Stress_Tol': ('var11', partial(min_max_scaler, [0, 40], [1, 5])),
-    'Sc_Adapt': ('var12', partial(min_max_scaler, [8, 40], [1, 5]))
+    "Sc_Fluency": ("var1", partial(min_max_scaler, [0, 30], [1, 5])),
+    "Sc_Induc_Reas": ("var2", partial(min_max_scaler, [0, 8], [1, 5])),
+    "score_matrices": ("var3", partial(min_max_scaler, [0, 1], [1, 5])),
+    "Sc_WM": ("var4", partial(min_max_scaler, [0, 12], [1, 5])),
+    "Sc_MOY_RT": ("var5", partial(min_max_scaler, [0, 5], [1, 5])),
+    "Sc_Verbal_Com": ("var6", partial(min_max_scaler, [0, 48], [1, 5])),
+    "score_PM": ("var7", partial(min_max_scaler, [0, 1], [1, 5])),
+    "score_metacog": ("var8", partial(min_max_scaler, [0, 1], [1, 5])),
+    "Sc_Leader": ("var9", partial(min_max_scaler, [6, 24], [1, 5])),
+    "Sc_Self_cont": ("var10", partial(min_max_scaler, [13, 65], [1, 5])),
+    "Sc_Stress_Tol": ("var11", partial(min_max_scaler, [0, 40], [1, 5])),
+    "Sc_Adapt": ("var12", partial(min_max_scaler, [8, 40], [1, 5])),
 }
 
 
 def process(df_main, df_cruiser):
-    df_main = df_main.loc[df_main['Finished'] != '0', :]
-    df_cruiser = df_cruiser.loc[df_cruiser['Finished'] != '0', :]
-    cols_SC = [(x, df_main[x].iloc[0]) for x in df_main.columns if 'SC' in x]
-    df_main = df_main.rename(
-        columns={before: after
-                 for before, after in cols_SC})
+    df_main = df_main.loc[df_main["Finished"] != "0", :]
+    df_cruiser = df_cruiser.loc[df_cruiser["Finished"] != "0", :]
+    cols_SC = [(x, df_main[x].iloc[0]) for x in df_main.columns if "SC" in x]
+    df_main = df_main.rename(columns={before: after for before, after in cols_SC})
     df_main = df_main.iloc[2:]
-    df_main = df_main.reset_index().set_index('ID')
-    df_cruiser = df_cruiser.reset_index().set_index('ID')
-    df_cruiser = df_cruiser.dropna()
-    df = pd.concat([df_main, df_cruiser], axis=1, join='inner')
-    df['Sc_Fluency'] = df['COG_VF1'].str.split().apply(
-        len) + df['COG_VF2'].str.split().apply(len)
-    df['Sc_Fluency'] *= 0.5
+    df_cruiser = df_cruiser.iloc[2:]
+    ##########
+    df_main = df_main.rename(columns={"id": "ID"})
+    ##########
+    df_cruiser = df_cruiser[df_cruiser["ID"].notnull()]
+    df_main = df_main.reset_index().set_index("ID")
+    df_cruiser = df_cruiser.reset_index().set_index("ID")
+    df_main = df_main.loc[~df_main.index.duplicated(keep="first")]
+    df_cruiser = df_cruiser.loc[~df_cruiser.index.duplicated(keep="first")]
+    df = pd.concat([df_main, df_cruiser], axis=1, join="inner")
+    df["Sc_Fluency"] = df["COG_VF1"].str.split().apply(len) + df[
+        "COG_VF2"
+    ].str.split().apply(len)
+    df["Sc_Fluency"] *= 0.5
 
     df = df[list(name_to_var.keys())]
-    df = df.apply(pd.to_numeric, errors='coerce')
+    df = df.apply(pd.to_numeric, errors="coerce")
     for col, (_, mapper) in name_to_var.items():
         df[col] = df[col].apply(mapper)
     df = df.reset_index()
     df = df.fillna(3)
+    print(df.shape)
+    print(df)
     return df
 
 
 def get_surveys():
     # Setting user Parameters
-    apiToken = 'T8o9qNI2J3hnl1TlMEcn2nLShZWW0Kj1ZvyoaLAf'
+    apiToken = "T8o9qNI2J3hnl1TlMEcn2nLShZWW0Kj1ZvyoaLAf"
 
     survey_id_main = "SV_6tmPFThjXFpKg17"
     survey_id_cruiser = "SV_01bq6G5QXO4Vt1r"
     fileFormat = "csv"
-    dataCenter = 'eu'
+    dataCenter = "eu"
 
     def dw_survey(surveyId):
         # Setting static parameters
         requestCheckProgress = 0.0
         progressStatus = "inProgress"
         baseUrl = "https://{0}.qualtrics.com/API/v3/surveys/{1}/export-responses/".format(
-            dataCenter, surveyId)
-        headers = {
-            "content-type": "application/json",
-            "x-api-token": apiToken,
-        }
+            dataCenter, surveyId
+        )
+        headers = {"content-type": "application/json", "x-api-token": apiToken}
 
         # Step 1: Creating Data Export
         downloadRequestUrl = baseUrl
-        downloadRequestPayload = json.dumps({
-            'format': fileFormat,
-        })
+        downloadRequestPayload = json.dumps({"format": fileFormat})
         downloadRequestResponse = requests.request(
-            "POST",
-            downloadRequestUrl,
-            data=downloadRequestPayload,
-            headers=headers)
-        #print(downloadRequestResponse.text)
+            "POST", downloadRequestUrl, data=downloadRequestPayload, headers=headers
+        )
+        # print(downloadRequestResponse.text)
         print(downloadRequestResponse.json())
         progressId = downloadRequestResponse.json()["result"]["progressId"]
-        #print(downloadRequestResponse.text)
+        # print(downloadRequestResponse.text)
 
         # Step 2: Checking on Data Export Progress and waiting until export is ready
         while progressStatus != "complete" and progressStatus != "failed":
-            #print ("progressStatus=", progressStatus)
+            # print ("progressStatus=", progressStatus)
             requestCheckUrl = baseUrl + progressId
             requestCheckResponse = requests.request(
-                "GET", requestCheckUrl, headers=headers)
-            requestCheckProgress = requestCheckResponse.json(
-            )["result"]["percentComplete"]
-            #print("Download is " + str(requestCheckProgress) + " complete")
+                "GET", requestCheckUrl, headers=headers
+            )
+            requestCheckProgress = requestCheckResponse.json()["result"][
+                "percentComplete"
+            ]
+            # print("Download is " + str(requestCheckProgress) + " complete")
             progressStatus = requestCheckResponse.json()["result"]["status"]
 
-        #step 2.1: Check for error
+        # step 2.1: Check for error
         if progressStatus is "failed":
             raise Exception("export failed")
 
         fileId = requestCheckResponse.json()["result"]["fileId"]
 
         # Step 3: Downloading file
-        requestDownloadUrl = baseUrl + fileId + '/file'
+        requestDownloadUrl = baseUrl + fileId + "/file"
         requestDownload = requests.request(
-            "GET", requestDownloadUrl, headers=headers, stream=True)
+            "GET", requestDownloadUrl, headers=headers, stream=True
+        )
 
         z = zipfile.ZipFile(io.BytesIO(requestDownload.content))
         return z
 
     z_main = dw_survey(survey_id_main)
     z_cruiser = dw_survey(survey_id_cruiser)
-    data_main = z_main.read('COG-12.csv')
-    data_cruiser = z_cruiser.read('J4U - CRUISER.csv')
-    df_main = pd.read_csv(StringIO(str(data_main, 'utf-8')))
-    df_cruiser = pd.read_csv(StringIO(str(data_cruiser, 'utf-8')))
+    data_main = z_main.read("COG-12.csv")
+    data_cruiser = z_cruiser.read("J4U - CRUISER.csv")
+    df_main = pd.read_csv(StringIO(str(data_main, "utf-8")))
+    df_cruiser = pd.read_csv(StringIO(str(data_cruiser, "utf-8")))
     df = process(df_main, df_cruiser)
     return df
 
@@ -133,8 +137,8 @@ def retrieve_all(surveyId):
     df = get_surveys()
     uois = User.query.filter_by(formDone=False).all()
     for uoi in uois:
-        if uoi.surveyId in df['ID'].unique():
-            v = df[df['ID'] == uoi.surveyId][df.columns[1:]].values[0]
+        if uoi.surveyId in df["ID"].unique():
+            v = df[df["ID"] == uoi.surveyId][df.columns[1:]].values[0]
             f = Features(*v)
             uoi.features = f
             uoi.formDone = True
@@ -142,7 +146,7 @@ def retrieve_all(surveyId):
 
     db_session.commit()
 
-    if surveyId in df['ID'].unique():
+    if surveyId in df["ID"].unique():
         return True
     return False
 
